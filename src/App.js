d99429afd5caf29540fe113dc74847b40ab95f7e
Vijayanand R <vijaynandr2000@gmail.com>
2021-04-29 13:41:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Map from "./components/Map";
+// import MapView from "./components/MapView";
 
 function App() {
+  const [place, setPlace] = useState("");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+  const [list, setList] = useState([]);
+  const [check, setCheck] = useState(false);
+
+  const add = () => {
+    console.log(place.length);
+    if (!place) alert("Enter the Location");
+    else {
+      setCheck(!check);
+      var url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=c7a9299a3d8da1d910da08bcffb48a3b`;
+      // console.log(url);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data) alert("Place Not founded");
+          setLon(data.coord.lon);
+          setLat(data.coord.lat);
+
+          // console.log(a, b);
+        })
+        .catch((e) => alert(e.message));
+    }
+  };
+
+  const submit = () => {
+    // <Map lat={lat} lat={lon} />;
+    setCheck(!check);
+    setList([
+      ...list,
+      {
+        place,
+        lat,
+        lon,
+      },
+    ]);
+    setPlace("");
+    setLon("");
+    setLat("");
+  };
+  // console.log(list);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="container head">
+          <h1>Find the Location</h1>
+        </div>
+        <div className="card">
+          <div className="card-head">
+            <div className="card-title">
+              <div className="input">
+                <div className="location">
+                  <h6>Location</h6>
+                  <input
+                    type="text"
+                    class="form-control "
+                    placeholder="Location"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    value={place}
+                    onChange={(e) => {
+                      setPlace(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="location">
+                  <h6>Lattitude</h6>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Lattitude"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    value={lat}
+                  />
+                </div>
+                <div className="location">
+                  <h6>Longitude</h6>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Longitude"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    value={lon}
+                  />
+                </div>
+              </div>
+              <div className="button">
+                <button
+                  className={`btn add-btn`}
+                  onClick={() => {
+                    if (check) {
+                      // setCheck(!check);
+                      submit();
+                    } else {
+                      // setCheck(!check);
+                      add();
+                    }
+                  }}
+                >
+                  {check ? "Submit" : "Add"}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <div class="  list-card" style={{ width: 25 + "rem" }}>
+              <div class="card-body">
+                <table class="table table-bordered table-dark">
+                  <thead>
+                    <tr>
+                      <th scope="col">Location</th>
+                      <th scope="col">Lattitude</th>
+                      <th scope="col">Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {list.map((item) => (
+                      <tr>
+                        <td>{item.place}</td>
+                        <td>{item.lat}</td>
+                        <td>{item.lon}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                  <div className="btn">
+                    <button className="btn btn-danger mt-3 route-btn text-center">
+                      Show Route
+                    </button>
+                  </div>
+                </table>
+              </div>
+            </div>
+            <div className=" card  map-card" style={{ width: 40 + "rem" }}>
+              {<Map list={list} />}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+const Map = ({ list }) => {
+  console.log(list);
+  const [viewport, setViewport] = useState({
+    latitude: 13.0878,
+    longitude: 80.2785,
+    zoom: 8,
+  });
+
+  return (
+    <ReactMapGL
+      mapboxApiAccessToken="pk.eyJ1IjoidmlqYXlhbmFuZHIiLCJhIjoiY2tvMmpnY3pmMDhucjJ2cnR6bm1lbGJsNiJ9.rEHL1kc21r7U7ciJHXcN7w"
+      {...viewport}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      width="100%"
+      height="90%"
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      {list.map((item) => (
+        <Marker
+          latitude={item.lat}
+          longitude={item.lon}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F299087%2Fmarker_map_icon&psig=AOvVaw1Bt1ZA0ZgwmhiARGtQHOzg&ust=1619767483865000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjriaL2ovACFQAAAAAdAAAAABAJ"
+            alt="here is the place"
+            width={viewport.zoom * 5}
+            height={viewport.zoom * 5}
+            color="red"
+          />
+        </Marker>
+      ))}
+    </ReactMapGL>
+  );
+};
+export default Map;
